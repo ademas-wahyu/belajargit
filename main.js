26500438f75f28a55e7237acb3c9de2140fc4777
theclaw-sts <wahyusiakrom238@gmail.com>
2024-02-09 21:14:56 +0700

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+const fruits = ['banana', 'mango', 'apple', 'orange'];
+fruits.push('lemon');
+fruits.forEach(buah);
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+const buahan = typeof fruits;
+console.log(buahan);
+
+function buah(value) {
+    console.log(value);
+}
